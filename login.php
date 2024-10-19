d956992256cd11a6367a76945c5672a251f7003e
Pedro Castro <vieiracastropedro7@gmail.com>
2024-10-19 14:48:30 -0300

    Adicionei o nome do site à página de login

--- login.php
+++ login.php
@@ -14,6 +14,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
+
+    <div id="titulo-login">
+        <h1>
+            Work<span>Done</span>
+        </h1>
+    </div>
+
     <div class="wrapper">
         <div class="title-text">
             <div class="title login">
